3abe31b93ca17542f75f0cf5bd8f4fe0288db30a
mounir2103 <mounoumounou2021@gmail.com>
2026-08-27 21:20:03 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
 
-        String command = scanner.nextLine();
-
-        System.out.println(command + ": command not found");
+        while(true){
+            System.out.print("$ ");
+            String command = scanner.nextLine();
+            System.out.println(command + ": command not found");
+        }
+        
     }
 }

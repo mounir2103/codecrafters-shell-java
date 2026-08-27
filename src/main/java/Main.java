f299e0e8a9157b320b29ef69531ddd6577fadc9d
mounir2103 <mounoumounou2021@gmail.com>
2026-08-27 22:57:06 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,7 +17,7 @@ public class Main {
                 if (builtin.contains(command.substring(5))) {
                     System.out.println(command.substring(5) + " is a shell builtin");
                 }else{
-                    System.out.println(command.substring(5) + " command not found");
+                    System.out.println(command.substring(5) + ": not found");
                 }
             }else{
                 System.out.println(command + ": command not found");
